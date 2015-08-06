ea2e7a50abbc32ac17dbae5741c4d497f52b16d1
Jochen Klar <jochenklar@gmail.com>
2015-08-06 09:05:13 +0200

    urls updated

--- wbh/urls.py
+++ wbh/urls.py
@@ -2,21 +2,28 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView,RedirectView
 from django.contrib import admin
 
+from wbc.process.views import PlaceCreate,PlaceUpdate,PlaceDelete
+from wbc.process.views import PublicationCreate,PublicationUpdate,PublicationDelete
 from wbc.process.views import PublicationFeed
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='core/map.html')),
-    url(r'^begriffe/$', 'wbc.process.views.process'),
+    url(r'^begriffe/$', 'wbc.process.views.process', name='process'),
     url(r'^liste/$', TemplateView.as_view(template_name='process/list.html')),
 
     # orte
     url(r'^orte/$', RedirectView.as_view(url='/liste/', permanent=True)),
-    url(r'^orte/(?P<pk>\d+)/$', 'wbc.process.views.place'),
+    url(r'^orte/neu/$', PlaceCreate.as_view(), name='place_create'),
+    url(r'^orte/(?P<pk>[0-9]+)/$', 'wbc.process.views.place', name='place'),
+    url(r'^orte/(?P<pk>[0-9]+)/bearbeiten/$', PlaceUpdate.as_view(), name='place_update'),
+    url(r'^orte/(?P<pk>[0-9]+)/entfernen/$', PlaceDelete.as_view(), name='place_delete'),
 
     # veroeffentlichungen neu
-    url(r'^veroeffentlichungen/neu/$', 'wbc.process.views.create_publication'),
+    url(r'^veroeffentlichungen/neu/$', PublicationCreate.as_view(), name='publication_create'),
+    url(r'^veroeffentlichungen/(?P<pk>[0-9]+)/bearbeiten/$', PublicationUpdate.as_view(), name='publication_update'),
+    url(r'^veroeffentlichungen/(?P<pk>[0-9]+)/entfernen/$', PublicationDelete.as_view(), name='publication_delete'),
 
     # feeds
     url(r'^feeds/$', 'wbc.core.views.feeds'),
@@ -36,8 +43,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # user login
-    # url(r'^login/', 'wbc.core.views.login_user'),
-    # url(r'^logout/', 'wbc.core.views.logout_user'),
+    url(r'^login/', 'wbc.core.views.login_user'),
+    url(r'^logout/', 'wbc.core.views.logout_user'),
 
     # robots.txt and sitemap.xml
     (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
