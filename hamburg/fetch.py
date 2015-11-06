060cf1324f22c3064d7abc014811881e5bf472f3
Jochen Klar <jochenklar@gmail.com>
2015-11-06 09:22:53 +0100

    wbh renamed to hamburg

---
+++ hamburg/fetch.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+import os,subprocess,json,urllib2,time,sys,re
+
+from wbc.region.models import Quarter
+from wbc.projects.models import Project
+
+class ProjectFetcher():
+
+    def fetch(self):
+
+        umlaut_quarters = {
+            "Barmbek-Sued": "Barmbek-Süd",
+            "Fuhlsbuettel": "Fuhlsbüttel",
+            "Lohbruegge": "Lohbrügge",
+            "Poppenbuettel": "Poppenbüttel",
+            "Suelldorf": "Sülldorf",
+            "Wellingsbuettel": "Wellingsbüttel"
+        }
+
+        # get time since last download
+        try:
+            time_delta = time.time() - os.stat('/tmp/imverfahren.json').st_ctime
+        except OSError:
+            time_delta = 86400
+
+        # download data if older than 10 minutes
+        if time_delta > 600:
+            try:
+                os.remove('/tmp/imverfahren.json')
+            except OSError:
+                pass
+
+            # call org2org to fetch the bplan geojson from the FIZ-Broker
+            print "querying geodinste-hamburg.de WFS server"
+            cmd = 'ogr2ogr -overwrite -s_srs EPSG:25832 -t_srs WGS84 -f geoJSON /tmp/imverfahren.json WFS:"http://geodienste-hamburg.de/HH_WFS_Bebauungsplaene" app:imverfahren'
+            subprocess.call(cmd,shell=True);
+
+        # open geojson
+        geojson = json.load(open('/tmp/imverfahren.json','r'))
+        n = 0
+        for feature in geojson["features"]:
+
+            # prepare values dictionary
+            project_values = {}
+
+            # get identifier
+            try:
+                project_values['identifier'] = feature['properties']['plan'].replace(' ','').strip()
+                cleaned_identifier = project_values['identifier'].replace('Aend','')
+                quarters = []
+                for quarter in re.findall('([a-zA-Z][a-zA-Z\-\.]+)',cleaned_identifier):
+                    if quarter in umlaut_quarters:
+                        quarters.append(umlaut_quarters[quarter])
+                    else:
+                        quarters.append(quarter)
+                project_values['name'] = cleaned_identifier
+            except KeyError:
+                continue
+
+            # switch lat and lon in (multi) polygon and get center
+            try:
+                latMin,latMax,lonMin,lonMax = 90,-90,180,-180
+                if feature['geometry']['type'] == 'Polygon':
+                    for path in feature['geometry']['coordinates']:
+                        for point in path:
+                            point[0],point[1] = point[1],point[0]
+                            latMin = min(latMin,point[0])
+                            latMax = max(latMax,point[0])
+                            lonMin = min(lonMin,point[1])
+                            lonMax = max(lonMax,point[1])
+                    project_values['polygon'] = json.dumps([feature['geometry']['coordinates']])
+                else:
+                    for polygon in feature['geometry']['coordinates']:
+                        for path in polygon:
+                            for point in path:
+                                point[0],point[1] = point[1],point[0]
+                                latMin = min(latMin,point[0])
+                                latMax = max(latMax,point[0])
+                                lonMin = min(lonMin,point[1])
+                                lonMax = max(lonMax,point[1])
+                    project_values['polygon'] = json.dumps(feature['geometry']['coordinates'])
+
+                # get lat and lon
+                project_values['lat'] = str((latMax + latMin) * 0.5)
+                project_values['lon'] = str((lonMax + lonMin) * 0.5)
+            except TypeError:
+                continue
+
+            # see if is marked active
+            try:
+                if feature['properties']['feststellung'].lower() == 'ja':
+                    project_values['active'] = False
+                else:
+                    project_values['active'] = True
+            except KeyError:
+                project_values['active'] = True
+
+            # update the place or create a new one
+            project,created = Project.objects.update_or_create(identifier=project_values['identifier'],defaults=project_values)
+            if created:
+                n += 1
+                try:
+                    for quarter in quarters:
+                        q = Quarter.objects.get(name=quarter)
+                        project.entities.add(q)
+                        project.save()
+                except Quarter.DoesNotExist:
+                    print 'no quarter for', project_values['identifier']
+
+                if project.address == '':
+                    # get address from open street map
+                    url = "http://nominatim.openstreetmap.org/reverse?format=json&lat=%s&lon=%s&zoom=18&addressdetails=1" % (project.lat,project.lon)
+
+                    response = urllib2.urlopen(url).read()
+                    data = json.loads(response)
+                    if 'road' in data['address']:
+                        project.address = data['address']['road']
+                    else:
+                        project.address = ''
+                    time.sleep(1.2)
+                project.save()
+
+                print project,'(' + ', '.join([str(quarter) for quarter in quarters]) + ')'
+
+        print n,'projects created'
---
+++ hamburg/settings.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import os
+from local import *
+
+INSTALLED_APPS = (
+    # django
+    'django_admin_bootstrapped',
+    # 'autocomplete_light',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+    'django_extensions',
+    # other dependencies
+    'rest_framework',
+    'rest_framework_gis',
+    'widget_tweaks',
+    'markdown',
+    'compressor',
+    # we build city apps
+    'wbc.core',
+    'wbc.region',
+    'wbc.process',
+    'wbc.notifications',
+    'wbc.comments',
+    'wbc.stakeholder',
+    'wbc.tags',
+    'wbc.projects',
+    'wbc.events',
+    'wbc.accounts',
+    #'wbc.buildings'
+    # 'rolodex',
+    'sortedm2m',
+    'photologue',
+    # 'sorl.thumbnail',
+    'taggit',
+    'taggit_templatetags',
+    # 'taggit_labels',
+    'haystack'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(SITE_ROOT,'templates/')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'wbc.core.context_processors.settings'
+            ],
+        },
+    },
+]
+
+ROOT_URLCONF = 'hamburg.urls'
+WSGI_APPLICATION = 'hamburg.wsgi.application'
+SITE_ID = 1
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+LANGUAGE_CODE = 'de-de'
+TIME_ZONE = 'Europe/Berlin'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT,'media_root/')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(SITE_ROOT,'static_root/')
+
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT,'static/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+LOGIN_URL = '/login'
+
+FEED_TITLE = "Wir bauen Hamburg (Veröffentlichungen)"
+FEED_DESCRIPTION = "Veröffentlichungen zu Bauvorhaben in Hamburg"
+
+TILES_URL = 'http://{s}.tiles.we-build.city/hamburg/{z}/{x}/{y}.png'
+TILES_OPT = {
+    'attribution': 'Map data &copy; 2012 OpenStreetMap contributors',
+    'maxZoom': 17,
+    'minZoom': 10,
+    'zIndex': 0,
+    'reuseTiles': 1
+}
+
+DEFAULT_VIEW = {
+    'lat': 53.550556,
+    'lon': 10.0,
+    'zoom': 11
+}
