7a94a12b0ff3a7e45f34c4a1c35e384f4717ecaf
lundelius <timo@lundelius.com>
2016-06-27 14:31:21 +0200

    content edited

--- city/content_settings.py
+++ city/content_settings.py
@@ -3,18 +3,18 @@ from django.core.urlresolvers import reverse_lazy
 
 #GENERAL SETTINGS
 DETAILS_TABS = {
-    'info' : True,
-    '3d' : True,
+    'info' : False,
+    '3d' : False,
     'images' : True,
     'events' : True,
     'stakeholder' : True,
     'discussion' : True,
-    'map' : True,
+    'map' : False,
     'etherpad' : True,
 }
 
 ORDER_BTNS = [
-    {'value' : 'name', 'text' : 'Name'},
+    {'value' : 'name', 'text' : 'Ideen'},
     {'value' : '-created', 'text' : 'Datum'},
     # {'value' : '-num_stakeholder', 'text' : '# Beteiligte'},
     {'value' : '-ratings_avg', 'text' : 'Bewertung'},
@@ -23,14 +23,14 @@ ORDER_BTNS = [
 
 #DOOOOOOOOOOOOOOO THIS
 GENERAL_CONTENT = {
-    'blog'    : True,
-    'lexikon' : True,
+    'blog'    : False,
+    'lexikon' : False,
     'account' : True,
 }
 
 #TOP BOXES ON STARTPAGE
 STARTPAGE_OVERVIEW_ICONS = [
-    {'text' : 'Suche nach Ideen & Projekten', 'icon': 'fa-map-o', 'overlay': 'Finde 3000+ Baupläne', 'link' : reverse_lazy('search') },
+    {'text' : 'Suche nach Ideen & Projekten', 'icon': 'fa-map-o', 'overlay': 'Finde mehr als 70 Ideen Projekte', 'link' : reverse_lazy('search') },
     {'text' : 'Suche nach Ideen & Projekten', 'icon': 'fa-map-o', 'overlay': 'Finde 3000+ Baupläne', 'link' : reverse_lazy('search') },
     {'text' : 'Suche nach Ideen & Projekten', 'icon': 'fa-map-o', 'overlay': 'Finde 3000+ Baupläne', 'link' : reverse_lazy('search') },
 ]
