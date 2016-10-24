2999347c6179ce0598f0953c7e591f6c578f9c7b
lundelius <timo@lundelius.com>
2016-10-24 11:57:55 +0200

    startpaged edited

--- city/content_settings.py
+++ city/content_settings.py
@@ -42,19 +42,19 @@ STARTPAGE_OVERVIEW_ICONS = [
 
 #BOTTOM BOXES ON STARTPAGE
 STARTPAGE_TOPIC_ICONS = [
-    {'text' : 'Wirtschaft, Arbeits- und Ausbildungsplätze', 'icon': 'fa-line-chart', 'overlay': 'Wirtschaft, Arbeits- und Ausbildungsplätze', 'link' : reverse_lazy('search'), 'params' : 'tags=Wirtschaft'  },
+    {'text' : 'Wirtschaft', 'icon': 'fa-line-chart', 'overlay': 'Wirtschaft', 'link' : reverse_lazy('search'), 'params' : 'tags=Wirtschaft'  },
     {'text' : 'Mobilität', 'icon': 'fa-car', 'overlay': 'Mobilität', 'link' : reverse_lazy('search'), 'params' : 'tags=Mobilität' },
     {'text' : 'Bildung', 'icon': 'fa-graduation-cap', 'overlay': 'Bildungsideen', 'link' : reverse_lazy('search'), 'params' : 'tags=Bildung'  },
     {'text' : 'Familie', 'icon': 'fa-heart', 'overlay': 'Alles zum Thema Familie', 'link' : reverse_lazy('search'), 'params' : 'tags=Familie'  },
     {'text' : 'Infrastruktur', 'icon': 'fa-train', 'overlay': '', 'link' : reverse_lazy('search'), 'params' : 'tags=Infrastruktur'  },
-    {'text' : 'Kultur und Freizeit', 'icon': 'fa-coffee', 'overlay': 'Kulturprojekte und Ideen', 'link' : reverse_lazy('search'), 'params' : 'tags=Kultur'  },
-    {'text' : 'Zivilgesellschaft / Ehrenamt', 'icon': 'fa-university', 'overlay': 'Zivilgesellschaft / Ehrenamt', 'link' : reverse_lazy('search'), 'params' : 'tags=Zivilgesellschaft'  },
+    {'text' : 'Kultur', 'icon': 'fa-coffee', 'overlay': 'Kulturprojekte und Ideen', 'link' : reverse_lazy('search'), 'params' : 'tags=Kultur'  },
+    {'text' : 'Zivilgesellschaft', 'icon': 'fa-university', 'overlay': 'Zivilgesellschaft / Ehrenamt', 'link' : reverse_lazy('search'), 'params' : 'tags=Zivilgesellschaft'  },
     {'text' : 'Wohnen', 'icon': 'fa-home', 'overlay': 'Wohnen', 'link' : reverse_lazy('search'), 'params' : 'tags=Wohnen'  },
     {'text' : 'Gesundheit', 'icon': 'fa-heartbeat', 'overlay': 'Gesundheit', 'link' : reverse_lazy('search'), 'params' : 'tags=Gesundheit'  },
     {'text' : 'Zuwanderung (Migration)', 'icon': 'fa-universal-access', 'overlay': 'Zuwanderung (Migration)', 'link' : reverse_lazy('search'), 'params' : 'tags=Zuwanderung'  },
     {'text' : 'Alter', 'icon': 'fa-blind', 'overlay': 'Alter', 'link' : reverse_lazy('search'), 'params' : 'tags=Alter'  },
     {'text' : 'Demokratie & Partizipation', 'icon': 'fa-hand-paper-o', 'overlay': 'Demokratie & Partizipation', 'link' : reverse_lazy('search'), 'params' : 'tags=Demokratie'  },
-    {'text' : 'Öffentliche Verwaltung', 'icon': 'fa-university', 'overlay': 'Öffentliche Verwaltung', 'link' : reverse_lazy('search'), 'params' : 'tags=Verwaltung'  },
+    # {'text' : 'Öffentliche Verwaltung', 'icon': 'fa-university', 'overlay': 'Öffentliche Verwaltung', 'link' : reverse_lazy('search'), 'params' : 'tags=Verwaltung'  },
 
 ]
 
